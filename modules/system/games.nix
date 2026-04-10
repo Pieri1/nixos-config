e4fbcb2f3c274004c6e59a4ace2e3d419d2d8aa6
Pieri1 <joaopbs20111@gmail.com>
2026-04-10 16:37:00 -0300

    updated game.nix

--- modules/system/games.nix
+++ modules/system/games.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # 1. Habilita a Steam com otimizações
@@ -10,7 +10,16 @@
   };
 
   # 2. Gamemode: Otimiza CPU, GPU e prioridade de processo ao jogar
-  programs.gamemode.enable = true;
+  programs.gamemode = {
+    enable = true;
+    settings = {
+      general = {
+        renice = 10;
+        ioprio = 0;
+        inhibit_screensaver = 1;
+      };
+    };
+  };
 
   # 3. Gamescope: Micro-compositor para rodar jogos em resoluções customizadas ou HDR
   programs.gamescope.enable = true;
@@ -21,11 +30,48 @@
     enable32Bit = true; # Necessário para a maioria dos jogos Steam (32 bits)
   };
 
-  # 5. Pacotes auxiliares para a melhor experiência
+  # 5. Melhor comportamento em sistemas com pouca RAM
+  zramSwap = {
+    enable = true;
+    memoryPercent = 50;
+    algorithm = "zstd";
+  };
+
+  # 6. Tunagens de kernel úteis para jogos
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 10;
+    "vm.max_map_count" = 2147483642;
+  };
+
+  # 7. Pacotes auxiliares para a melhor experiência
   environment.systemPackages = with pkgs; [
     mangohud      # Overlay de FPS e temperatura (estilo o que você vê no Windows)
     protonup-qt   # Interface fácil para baixar o GE-Proton (essencial para rodar tudo)
     vulkan-tools  # Para testar se o driver vulkan está ok (vulkaninfo)
     winetricks    # Quebra-galho para dependências de jogos Windows
+    lact
   ];
+
+  # 8 Garante RADV no Vulkan quando lançar jogos
+  environment.sessionVariables = {
+    AMD_VULKAN_ICD = "RADV";
+  };
+
+  # 9 Remover/evitar governor fixo global:
+  # powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      # Na tomada: performance
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      ENERGY_PERF_POLICY_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_AC = "performance";
+
+      # Na bateria: economia
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      ENERGY_PERF_POLICY_ON_BAT = "power";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+    };
+  };
 }
